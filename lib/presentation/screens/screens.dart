a2253f2ec01327ad35905a917376afc2f613640e
Ignacio98 <dani.manchu1998@gmail.com>
2023-09-09 22:40:47 -0500

    Added export url in export file

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,8 @@
-export 'home/home_screen.dart';
+export 'animated/animated_screen.dart';
+export 'app_tutorial/app_tutorial_screen.dart';
 export 'button/button_screen.dart';
 export 'card/card_screen.dart';
+export 'home/home_screen.dart';
+export 'infinite_scroll/infinite_scroll_screen.dart';
+export 'progress/progress.dart';
+export 'snackbar/snackbar.dart';
